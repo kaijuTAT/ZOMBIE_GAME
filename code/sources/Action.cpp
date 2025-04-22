7efe12fdd4db8c7ec793ec2b49821d6270ba72da
kaijuTAT <141261500+kaijuTAT@users.noreply.github.com>
2025-04-22 14:10:58 +0800

    Update Action.cpp

--- code/sources/Action.cpp
+++ code/sources/Action.cpp
@@ -1,5 +1,4 @@
 // ---------- src/Action.cpp ----------
-// (如果 ActionInfo 构造函数在头文件中定义，则此文件为空或不存在)
 #include "../include/Action.hh" // <<<--- UPDATED INCLUDE
 
 ActionInfo::ActionInfo(ActionType t, int index) : type(t), targetIndex(index) {}
